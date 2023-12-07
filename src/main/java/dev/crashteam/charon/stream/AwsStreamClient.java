23ee69f824ae9771c4d2b86243d1b4e5727f7ad6
SageFreke <lenigrast@gmail.com>
2023-12-08 00:02:49 +0300

    add aws config condition

--- src/main/java/dev/crashteam/charon/stream/AwsStreamClient.java
+++ src/main/java/dev/crashteam/charon/stream/AwsStreamClient.java
@@ -5,12 +5,14 @@ import com.amazonaws.services.kinesis.model.PutRecordsRequest;
 import com.amazonaws.services.kinesis.model.PutRecordsRequestEntry;
 import com.amazonaws.services.kinesis.model.PutRecordsResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(value = "app.stream.publisher", havingValue = "AWS")
 public class AwsStreamClient {
 
     private final AmazonKinesis amazonKinesis;
