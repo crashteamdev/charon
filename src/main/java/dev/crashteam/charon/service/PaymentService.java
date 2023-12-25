66e81862ae8306608c69c3fe681c5e7ea3c1defa
SageFreke <lenigrast@gmail.com>
2023-12-25 22:49:53 +0300

    Delete unnecessary logs

--- src/main/java/dev/crashteam/charon/service/PaymentService.java
+++ src/main/java/dev/crashteam/charon/service/PaymentService.java
@@ -204,7 +204,6 @@ public class PaymentService {
 
         PromoCode promoCode = StringUtils.hasText(purchaseService.getPromoCode())
                 ? promoCodeService.getPromoCode(purchaseService.getPromoCode()) : null;
-        log.info("Protobuf code = {}. Found promocode = {}", purchaseService.getPromoCode(), promoCode);
 
         PaidServiceContext paidServiceContext = purchaseService.getPaidService().getContext();
         PaidService paidService = getPaidServiceFromContext(paidServiceContext);
